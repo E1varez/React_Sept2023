a37efb7e074ff6aeadf1d03eda0ab385971ca888
Damian Rydel <damianrydel91@gmail.com>
2024-06-22 11:20:06 +0200

    hello world

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Sept 2023
         </a>
       </header>
     </div>
